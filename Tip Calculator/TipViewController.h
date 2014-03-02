7e0512380be2fdd8bd8e3219c595e77b7a23ffd1
kellyxu1981 <kxu@yahoo-inc.com>
2014-03-02 00:38:58 -0800

    completed tip calculator app

---
+++ Tip Calculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  Tip Calculator
+//
+//  Created by Kelly Xu on 3/1/14.
+//  Copyright (c) 2014 iosbootcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
