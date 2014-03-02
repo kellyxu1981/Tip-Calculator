32da3a14257d209484598871cdafcc9e15815f58
Kelly Xu <kxu@thersstreams-lm.gateway.2wire.net>
2014-03-01 18:27:46 -0800

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Kelly Xu on 3/1/14.
+//  Copyright (c) 2014 iosbootcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tip Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tip Calculator
+//
+//  Created by Kelly Xu on 3/1/14.
+//  Copyright (c) 2014 iosbootcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
